31c6360646e3829a2cda926e396c5d2fc6d23460
Fan Guochao <dianguangyishan@yeah.net>
2015-10-09 14:49:30 +0800

    modigy again

--- testfirst.py
+++ testfirst.py
@@ -4,6 +4,9 @@ Created on Mon Sep 28 00:06:50 2015
 
 @author: VANCHAO
 """
+
+qd=0
+pd=0
 SERVICE=open("SERVICE.TXT")
 dic=dict()
 alpha=[chr(e) for e in range(65,91)]
@@ -16,11 +19,19 @@ for j in range(13):
         line.pop(1)
         line.pop(0)
         line[0]=float(line[0])
-        line[1]=float(line[1])
+        line[1]=int(float(line[1])*100)
         a.append(line)
     dic[chr(j+65)]=a
 del a
 SERVICE.close()
+
+REQ=open("REQ.TXT")
+line=REQ.readline().strip().split(',')
+temp=line[0].split('(')
+qd=float(temp[1])
+temp=line[1].split(')')
+pd=int(temp[0])
+REQ.close()
 
 #read the point from the file
 PROCESS=open("PROCESS.TXT")
@@ -45,19 +56,17 @@ del temp
 del line
 PROCESS.close()
 
-REQ=open("REQ.TXT")
-REQ.close()
+
 
 info=[[]]#0 represent quality and 1 represent price
 for e in dic2:
 	info.append(dic[e])
-
 f=[{0:1}]
 #algorithm for manipulating
 for i in range(1,len(dic2)+1):
     f.append({})
     for j in f[i-1]:
-        for k in range(500):
+        for k in range(len(info[i])):
             if  j + info[i][k][1] not in f[i]:
                 f[i][j + info[i][k][1]]=f[i-1][j] * info[i][k][0]
             else:
