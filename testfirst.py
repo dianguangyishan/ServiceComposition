e7b2596c4469846e8cc25eb9237126a83cfd4103
Fan Guochao <dianguangyishan@yeah.net>
2015-10-09 22:32:49 +0800

    first copy can executive

--- testfirst.py
+++ testfirst.py
@@ -4,9 +4,7 @@ Created on Mon Sep 28 00:06:50 2015
 
 @author: VANCHAO
 """
-
-qd=0
-pd=0
+from decimal import *
 SERVICE=open("SERVICE.TXT")
 dic=dict()
 alpha=[chr(e) for e in range(65,91)]
@@ -18,8 +16,8 @@ for j in range(13):
         line.pop(3)
         line.pop(1)
         line.pop(0)
-        line[0]=float(line[0])
-        line[1]=int(float(line[1])*100)
+        line[0]=Decimal(line[0])
+        line[1]=Decimal(line[1])
         a.append(line)
     dic[chr(j+65)]=a
 del a
@@ -28,9 +26,9 @@ SERVICE.close()
 REQ=open("REQ.TXT")
 line=REQ.readline().strip().split(',')
 temp=line[0].split('(')
-qd=float(temp[1])
+qd=Decimal(temp[1])
 temp=line[1].split(')')
-pd=int(temp[0])
+pd=Decimal(temp[0])
 REQ.close()
 
 #read the point from the file
@@ -61,19 +59,21 @@ PROCESS.close()
 info=[[]]#0 represent quality and 1 represent price
 for e in dic2:
 	info.append(dic[e])
-f=[{0:1}]
+f=[{Decimal('0'):Decimal('1')}]
 #algorithm for manipulating
 for i in range(1,len(dic2)+1):
     f.append({})
     for j in f[i-1]:
         for k in range(len(info[i])):
-            if j + info[i][k][1] > pd *100:
+            if j + info[i][k][1] > pd:
+                continue
+            else:
+                tmp=(f[i-1][j] * info[i][k][0]).quantize(Decimal('1.00'))
+            if tmp < qd:
                 continue
             elif j + info[i][k][1] not in f[i]:
-                f[i][j + info[i][k][1]]=f[i-1][j] * info[i][k][0]
-            else:
-                tmp=f[i-1][j] * info[i][k][0]
-                if tmp > f[i][j + info[i][k][1]]:
+                f[i][j + info[i][k][1]]=tmp
+            elif tmp > f[i][j + info[i][k][1]]:
                     f[i][j + info[i][k][1]]=tmp
             
                 
